1d4fd23ae4550e2d3cd6afa0fbb8bc3ca3308a31
Rinat Enikeev <rinat.enikeev@gmail.com>
2024-11-10 10:15:06 +0200

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const WebsiteName: string = "Programyzer"
+export const WebsiteBaseUrl: string = "https://programyzer.com"
+export const WebsiteDescription: string =
+  "Programming Is More Than Coding: Comprehensive Solutions and Services for Development Challenges."
+export const CreateProfileStep: boolean = true
