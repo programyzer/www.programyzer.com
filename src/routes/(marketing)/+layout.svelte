7f84b12f294942ded3b1cfc374c182f719c5204c
gramyzer <contact@programyzer.com>
2026-02-28 22:38:47 +0200

    Translate header and footer from Russian to English (#3)
    * Translate header and footer from Russian to English
    
    * Apply prettier formatting

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -14,9 +14,7 @@
           class="border border-primary"
           data-cal-link="enikeev/15min"
           data-cal-namespace="15min"
-          >Записаться на звонок <span class="text-sm opacity-70"
-            >(бесплатно)</span
-          ></button
+          >Book a call <span class="text-sm opacity-70">(free)</span></button
         >
       </li>
       <!-- <li class="md:mx-2"><a href="/blog">Blog</a></li>
@@ -68,7 +66,7 @@
           <button
             class="border border-primary"
             data-cal-link="enikeev/15min"
-            data-cal-namespace="15min">Записаться на звонок</button
+            data-cal-namespace="15min">Book a call</button
           >
         </li>
       </ul>
@@ -88,19 +86,18 @@
     class="footer p-10 gap-x-48 lg:gap-x-64 xl:gap-x-96 place-content-center text-base"
   >
     <nav>
-      <span class="footer-title opacity-80">О проекте</span>
+      <span class="footer-title opacity-80">About</span>
       <!-- <a class="link link-hover mb-1" href="/">Overview</a> -->
       <!-- <a class="link link-hover my-1" href="/pricing">Pricing</a>
       <a class="link link-hover my-1" href="/blog">Blog</a> -->
       <!-- <a class="link link-hover my-1" href="/contact_us">Contact Us</a> -->
       <a
         class="link link-hover my-1"
-        href="https://github.com/programyzer/www.programyzer.com"
-        >Исходный код</a
+        href="https://github.com/programyzer/www.programyzer.com">Source code</a
       >
     </nav>
     <aside>
-      <span class="footer-title opacity-80">Благодарности</span>
+      <span class="footer-title opacity-80">Credits</span>
       <a
         class="link link-hover max-w-[260px]"
         href="https://saasstarter.work/"
